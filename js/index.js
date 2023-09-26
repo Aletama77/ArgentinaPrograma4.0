c150e4ae75ae3dfcc766f00fbf9c09c6335b2355
Alex77 <alexandertamashiro@gmail.com>
2023-09-26 14:44:45 -0300

    Es el primer commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const reservaButton = document.getElementById("reservaButton");
+    reservaButton.addEventListener("click", () => {
+        console.log("Gracias por reservar");
+    });
+});
